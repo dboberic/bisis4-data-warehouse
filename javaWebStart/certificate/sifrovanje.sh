a912327a708ccdecdaba825aad42abefe8d078ac
Branko Milosavljevic <mbranko@uns.ac.rs>
2016-02-05 17:33:48 +0100

    Pocetna verzija projekta.

---
+++ javaWebStart/certificate/sifrovanje.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+##
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra antlr-2.7.6.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra asm.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra axis.jar bisis4 
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra c3p0-0.9.1.jar bisis4 
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra cglib-2.2.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-beanutils.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-betwixt-0.7.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-codec-1.3.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-collections-3.2.jar bisis4
+ 
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-dbcp-1.2.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-digester-1.7.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-fileupload-1.2.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-httpclient-3.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-io-1.4.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-jxpath-1.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-lang-2.2.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-logging-1.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-pool-1.3.jar bisis4
+ 
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra commons-validator-1.3.0.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra dom4j-1.6.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra forms-1.0.7.jar bisis4 
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra freemarker.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra gint-utils.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra hessian-3.0.20.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra hibernate3.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra hibernate-jpa-2.0-api-1.0.1.Final.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra incubator-activemq-4.0.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra iText-2.1.7.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jakarta-oro-2.0.8.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jasperreports-5.5.0.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jasperreports-fonts-5.5.0.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra javassist-3.12.0.GA.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jaxm-api.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jaxm-runtime.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jaxp-api.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jcalendar-1.3.2.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jdbc2_0-stdext.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jsr173_1.0_api.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jta-1.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra log4j-1.2.11.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra looks-2.1.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra lucene-core-2.2.0.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra mysql-connector-java-5.1.6-bin.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra options.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra PDFBox-0.7.3.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra report.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra slf4j-api-1.6.4.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra slf4j-simple-1.6.4.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra swingmiglayout15.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra validator.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra warnings.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra xalan.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra xbean.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra xercesImpl.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra xml-apis.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra xmlpublic.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra jaxen-1.1.1.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra servlet-api-2.4.jar bisis4
+
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra bisis-textsrv.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra bisis-client.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra bisis-config.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra bisis-records.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra bisis-utils.jar bisis4
+
+jarsigner -keystore BISISKeystore -storepass bisisver4 -keypass bisissifra bisis-reports.jar bisis4
